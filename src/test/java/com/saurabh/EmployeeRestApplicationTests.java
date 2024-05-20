143ab6983a5e51c3dd09d7ef6d247d17d8655bc3
Saurabhjava <45967591+Saurabhjava@users.noreply.github.com>
2024-05-20 12:18:50 +0530

    added test cases

--- src/test/java/com/saurabh/EmployeeRestApplicationTests.java
+++ src/test/java/com/saurabh/EmployeeRestApplicationTests.java
@@ -1,13 +1,22 @@
 package com.saurabh;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.saurabh.model.Employee;
+import com.saurabh.service.EmployeeService;
 
 @SpringBootTest
 class EmployeeRestApplicationTests {
-
+	@Autowired
+	private EmployeeService eService;
 	@Test
 	void contextLoads() {
+		Employee emp=eService.getEmployee(1002);
+		assertEquals("Mark1", emp.getName());
 	}
 
 }
